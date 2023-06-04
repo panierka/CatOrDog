805e00467ed4e750aa0d801a8c6f0d4583bdf7d8
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-06-04 23:50:32 +0200

    feat: vue frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transpileDependencies: ['@vue/cli-service'],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+};
