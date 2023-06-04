46f4d28b056ff9746219acf04c629fc339a33307
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-06-04 23:37:22 +0200

    feat: basic routing handler

---
+++ backend/src/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from logger import log
+
+app = Flask(__name__)
+
+
+@app.get('/test')
+def test():
+    return {
+        'message': 'test'
+    }
+
+
+if __name__ == '__main__':
+    log.info('starting the server...')
+    app.run()
